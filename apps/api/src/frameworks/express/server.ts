34a3b7c23a7dfaf58a67cceb93b90bf71cdf205d
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-10-15 17:43:03 +0200

    feat(api): :sparkles: add basic server functionality
    Added basic Express server functionality and tested CORS origin for development.
     Rewrote main initialization logic to initialize the Express server during startup.
    
     - Set up Express with basic routes and middleware.
     - Added CORS configuration for development (allowing all origins).
     - Refactored main entry point to include server initialization logic.

---
+++ apps/api/src/frameworks/express/server.ts
@@ -0,0 +1,38 @@
+import express, { Express, Request, Response } from 'express';
+import cors, { CorsOptions } from 'cors';
+
+export const createServer = () => {
+  const app: Express = express();
+
+  const corsOptions: CorsOptions = {
+    origin: '*',
+  };
+
+  app.use(cors(corsOptions));
+  app.use(express.json({ limit: '5mb' }));
+  app.use(express.urlencoded({ extended: true }));
+
+  app.get('/', (req: Request, res: Response) => {
+    res.send('<h1>Lockify API</h1>');
+  });
+
+  /* Routes
+    here
+  */
+
+  app.use('*', (req: Request, res: Response) => {
+    res.status(404).send(`<h1>404! Resource not found!</h1>`);
+  });
+
+  /* Global error handler
+    here
+  */
+
+  return app;
+};
+
+export const startServer = (app: Express, PORT: number | string): void => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
