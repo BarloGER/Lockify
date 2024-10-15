a58c9470c88607089e0003e99c4e51aa86901fe0
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-10-15 17:43:03 +0200

    feat(api): :sparkles: add basic server functionality
    Added basic Express server functionality and tested CORS origin for development.
     Rewrote main initialization logic to initialize the Express server during startup.
    
     - Set up Express with basic routes and middleware.
     - Added CORS configuration for development (allowing all origins).
     - Refactored main entry point to include server initialization logic.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,21 +1,15 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+import { createServer, startServer } from './frameworks/express/server';
 
-import express from 'express';
-import * as path from 'path';
+const initializeServer = async () => {
+  try {
+    const PORT: number = Number(process.env.PORT) || 8080;
+    const app = createServer();
 
-const app = express();
+    startServer(app, PORT);
+  } catch (error) {
+    console.error('Failed to initialize server:', error);
+    process.exit(1);
+  }
+};
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
-
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api!' });
-});
-
-const port = process.env.PORT || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
-});
-server.on('error', console.error);
+initializeServer();
