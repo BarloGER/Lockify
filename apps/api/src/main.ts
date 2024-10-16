c73caf69e6c71c358e566cf0d8a0aa764e8322d7
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-10-16 12:26:23 +0200

    feat(api): :sparkles: add localization to api
    Added localization via i18next with http-middleware and fs-backend
    
     - Wrote a function to initialize i18next with Backend and LanguageDetector
     - Added middleware to use in server.ts
     - Initialized i18next in main.ts
     - Added simple locales to translate text for routes "/" and "*"

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,15 @@
 import { createServer, startServer } from "./frameworks/express/server";
+import { initializeI18n } from "./frameworks/i18next";
+
+console.log("NODE_ENV", process.env.NODE_ENV);
 
 const initializeServer = async () => {
   try {
     const PORT: number = Number(process.env.PORT) || 8080;
+
+    await initializeI18n();
+
+    // Create express server
     const app = createServer();
 
     startServer(app, PORT);
