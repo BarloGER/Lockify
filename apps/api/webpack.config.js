492433c6067ca14e2127d5c727c43b1c2aac3085
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-10-16 12:29:45 +0200

    chore(api): :wrench: update webpack config to copy locales into dist folder
    Updated the webpack config because locales could not be found in production build.
     Now the locales will be copied into Lockify/dist/apps/api

--- apps/api/webpack.config.js
+++ apps/api/webpack.config.js
@@ -1,20 +1,25 @@
-const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
-const { join } = require('path');
+const { NxAppWebpackPlugin } = require("@nx/webpack/app-plugin");
+const { join } = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   output: {
-    path: join(__dirname, '../../dist/apps/api'),
+    path: join(__dirname, "../../dist/apps/api"),
   },
   plugins: [
     new NxAppWebpackPlugin({
-      target: 'node',
-      compiler: 'tsc',
-      main: './src/main.ts',
-      tsConfig: './tsconfig.app.json',
-      assets: ['./src/assets'],
+      target: "node",
+      compiler: "tsc",
+      main: "./src/main.ts",
+      tsConfig: "./tsconfig.app.json",
+      assets: ["./src/assets"],
       optimization: false,
-      outputHashing: 'none',
+      outputHashing: "none",
       generatePackageJson: true,
+    }),
+    // Kopiere die Übersetzungsdateien in den `dist`-Ordner
+    new CopyWebpackPlugin({
+      patterns: [{ from: "src/frameworks/i18next/locales", to: "locales" }],
     }),
   ],
 };
