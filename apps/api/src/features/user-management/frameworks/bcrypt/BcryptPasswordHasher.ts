1b84c7b159feeaccfe3e2bcb3747829c63de1e5a
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-10-18 11:24:22 +0200

    refactor(api): :goal_net: add env and salt value validation
    Added env validation to avoid errors.
     Added salt round value validation to make sure it is an positive integer.

--- apps/api/src/features/user-management/frameworks/bcrypt/BcryptPasswordHasher.ts
+++ apps/api/src/features/user-management/frameworks/bcrypt/BcryptPasswordHasher.ts
@@ -2,9 +2,24 @@ import { IPasswordHasher } from "../../services/IPasswordHasher";
 import bcrypt from "bcrypt";
 
 export class BcryptPasswordHasher implements IPasswordHasher {
+  private readonly SALT_ROUNDS: number;
+
+  constructor() {
+    const saltRoundsEnv = process.env.SALT_ROUNDS;
+    if (!saltRoundsEnv) {
+      throw new Error("SALT_ROUNDS environment variable is not set!");
+    }
+
+    const saltRounds = parseInt(saltRoundsEnv, 10);
+    if (isNaN(saltRounds) || saltRounds <= 0) {
+      throw new Error("SALT_ROUNDS must be a positive integer");
+    }
+
+    this.SALT_ROUNDS = saltRounds;
+  }
+
   async hash(password: string): Promise<string> {
-    const SALT_ROUNDS: number = parseInt(process.env.SALT_ROUNDS);
-    return bcrypt.hash(password, SALT_ROUNDS);
+    return bcrypt.hash(password, this.SALT_ROUNDS);
   }
 
   async compare(password: string, hash: string): Promise<boolean> {
