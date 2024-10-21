d93f1bc21e069490b5c627b03e2b23db30c8e660
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-10-21 16:20:29 +0200

    feat(api): :sparkles: add registration functionality to api
    Completed user registration functionality for now. It allows a guest to register an account, if he passes validation. The entry point is the user router with its /register endpoint.
    
     - Added user router and some other things to the server.ts
     - Added translation keys.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,17 +1,20 @@
 import { createServer, startServer } from "./frameworks/express/server";
 import { initializeI18n } from "./frameworks/i18next";
+import { validateEnv } from "./configs/validateEnv";
 
 console.log("NODE_ENV", process.env.NODE_ENV);
 
 const initializeServer = async () => {
   try {
-    const PORT: number = Number(process.env.PORT) || 8080;
+    // Validates if every defined env variable is available
+    validateEnv();
 
+    // Initializes localization
     await initializeI18n();
 
     // Create express server
     const app = createServer();
-
+    const PORT: number = Number(process.env.PORT) || 8080;
     startServer(app, PORT);
   } catch (error) {
     console.error("Failed to initialize server:", error);
