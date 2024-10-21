b9ec62f5e5ea96cfdd8976139d9655a47b82b826
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-10-21 16:20:29 +0200

    feat(api): :sparkles: add registration functionality to api
    Completed user registration functionality for now. It allows a guest to register an account, if he passes validation. The entry point is the user router with its /register endpoint.
    
     - Added user router and some other things to the server.ts
     - Added translation keys.

--- apps/api/src/frameworks/express/server.ts
+++ apps/api/src/frameworks/express/server.ts
@@ -1,6 +1,9 @@
 import express, { Express, Request, Response } from "express";
 import cors, { CorsOptions } from "cors";
 import { i18nMiddleware } from "./middlewares/i18nMiddleware";
+import { userRouter } from "../../features/user-management/frameworks/express/routes/userRouter";
+import { sessionConfig } from "../express-session/sessionConfig";
+import { errorHandler } from "./middlewares/errorHandler";
 
 export const createServer = () => {
   const app: Express = express();
@@ -13,24 +16,21 @@ export const createServer = () => {
   app.use(express.json({ limit: "5mb" }));
   app.use(express.urlencoded({ extended: true }));
   app.use(i18nMiddleware);
+  app.use(sessionConfig);
 
   app.get("/", (req: Request, res: Response) => {
     const greeting = req.t("greeting");
     res.send(`<h1>${greeting}</h1>`);
   });
 
-  /* Routes
-    here
-  */
+  app.use("/user", userRouter);
 
   app.use("*", (req: Request, res: Response) => {
     const notFoundMessage = req.t("notFound");
     res.status(404).send(`<h1>404! ${notFoundMessage}</h1>`);
   });
 
-  /* Global error handler
-    here
-  */
+  app.use(errorHandler);
 
   return app;
 };
