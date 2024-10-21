7a99d8443991cf89da20f771dd9910ae2f7353cd
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-10-21 12:39:30 +0200

    refactor(api): :art: move files and use new custom errors

--- apps/api/src/frameworks/express-session/sessionConfig.ts
+++ apps/api/src/frameworks/express-session/sessionConfig.ts
@@ -1,7 +1,13 @@
 import session from "express-session";
 import { sessionStore } from "./sessionStore";
 
+const sessionSecretsEnv = process.env.SESSION_SECRETS;
+if (!sessionSecretsEnv) {
+  throw new Error("SESSION_SECRETS environment variable is not set");
+}
+
 const sessionSecrets = process.env.SESSION_SECRETS.split(", ");
+const isProduction = process.env.NODE_ENV === "production";
 
 export const sessionConfig = session({
   store: sessionStore,
@@ -10,8 +16,8 @@ export const sessionConfig = session({
   saveUninitialized: false,
   cookie: {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production" ? true : false,
+    secure: isProduction,
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-    sameSite: "none",
+    sameSite: isProduction ? "lax" : "strict",
   },
 });
