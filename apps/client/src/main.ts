7e99a3dac85095c73b471b99daf7837d6a410ecd
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-10-21 16:21:21 +0200

    feat(client): :sparkles: add environment variable validation to main

--- apps/client/src/main.ts
+++ apps/client/src/main.ts
@@ -1,9 +1,15 @@
 import { initializeApp } from "./frameworks/react/App";
 import { initializeI18n } from "./frameworks/i18next";
+import { validateEnv } from "./configs/validateEnv";
 
 const initializeClient = async (): Promise<void> => {
   try {
+    // Validates if every defined env variable is available
+    validateEnv();
+
+    // Initializes localization
     await initializeI18n();
+
     initializeApp();
   } catch (error) {
     console.error("Failed to initialize the client app:", error);
